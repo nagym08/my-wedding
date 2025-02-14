71d451376fcc2e26aeb305d8b533b7edc3b597d8
nagym08 <nagymiki08@gmail.com>
2025-02-14 14:34:04 +0100

    add eslint and prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
-import './App.css'
-import Photo from './components/Photo'
-import Rvsp from './components/Rvsp'
-import Schedule from './components/Schedule'
-import LocationInfo from './components/LocationInfo'
+import "./App.css"
+import Photo from "./components/Photo"
+import Rvsp from "./components/Rvsp"
+import Schedule from "./components/Schedule"
+import LocationInfo from "./components/LocationInfo"
 
 function App() {
-
   return (
     <>
       <nav className="header">
-      <a href="#schedule">Menetred</a>
-      <a href="#location">Helyszín</a>
-      <a href="#rvsp">RVSP</a>
-      <a href="#photo">Fotó</a>
-    </nav>
-    <main>
-      <section id="schedule"><Schedule/></section>
-      <section id="location"><LocationInfo/></section>
-      <section id="rvsp"><Rvsp/></section>
-      <section id="photo"><Photo/></section>
-    </main>
+        <a href="#schedule">Menetred</a>
+        <a href="#location">Helyszín</a>
+        <a href="#rvsp">RVSP</a>
+        <a href="#photo">Fotó</a>
+      </nav>
+      <main>
+        <section id="schedule">
+          <Schedule />
+        </section>
+        <section id="location">
+          <LocationInfo />
+        </section>
+        <section id="rvsp">
+          <Rvsp />
+        </section>
+        <section id="photo">
+          <Photo />
+        </section>
+      </main>
     </>
   )
 }
--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -1,17 +1,17 @@
-import 'react'
+import "react"
 
 function Schedule() {
   return (
-    <div className='section'>
-    <h1>Menetrend</h1>
+    <div className="section">
+      <h1>Menetrend</h1>
       <h2>2025. 09. 06.</h2>
       <ul>
-          <li>16:00 vendégvárás</li>
-          <li>17:00 szertás</li>
-          <li>18:00 grautláció</li>
-          <li>19:30 vacsora</li>
-          <li>23:00 torta</li>
-          <li>00:00 menyecsketánc</li>
+        <li>16:00 vendégvárás</li>
+        <li>17:00 szertás</li>
+        <li>18:00 grautláció</li>
+        <li>19:30 vacsora</li>
+        <li>23:00 torta</li>
+        <li>00:00 menyecsketánc</li>
       </ul>
       <span>Maps pin comes here</span>
     </div>
